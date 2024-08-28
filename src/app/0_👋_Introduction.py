2874c609951b4eb68ae17f4315b498c20fb9111b
Murat Saglam <muratusaglam@gmail.com>
2024-08-28 19:26:28 +0100

    Fix

--- src/app/0_👋_Introduction.py
+++ src/app/0_👋_Introduction.py
@@ -6,10 +6,5 @@ st.set_page_config(layout="wide")
 
 def read_markdown_file(file_path: Path) -> str:
     return file_path.read_text()
-
-
-st.title("Financial Engineering ")
-
-
 intro_md = read_markdown_file(file_path=Path("./README.md"))
 st.markdown(intro_md)
